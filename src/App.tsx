0e11a397a210e9c35362c9940414ee4216371599
yarqui <y.pelykh@gmail.com>
2024-05-14 13:55:42 -0400

    add svg icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Icon from "./components/Icon";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-2xl text-red-600">Camper</h1>
+      <h1 className="text-2xl text-red-600">Test Icon</h1>
+      <Icon className="fill-yellow-600" iconId="icon-calendar" />
     </div>
   );
 };
